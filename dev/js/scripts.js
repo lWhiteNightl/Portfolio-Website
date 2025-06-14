7ec5724eee2fcaace43d48c339e3006e450d7a99
Ruby Xu <139299636+lWhiteNightl@users.noreply.github.com>
2025-06-13 22:54:00 -0400

    add greenbar

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,4 +1,19 @@
+import {Fancybox} from '@fancyapps/ui';
+import "@fancyapps/ui/dist/fancybox/fancybox.css";
 import AOS from 'aos';
 import 'aos/dist/aos.css'; // You can also use <link> for styles
 // ..
 AOS.init();
+
+Fancybox.bind("[data-fancybox]", {
+});
+
+let greenBar = document.querySelector('#green-bar');
+let closeBarBtn = document.querySelector('#close-bar-btn');
+
+closeBarBtn.addEventListener('click', hideGreenBar);
+
+function hideGreenBar() {
+    greenBar.style.display = 'none';
+    localStorage.setItem('greenBarClosed', 'true');
+}
