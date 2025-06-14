4fd8b9c8f7a30ddd6c61d383d8bc23704b6b919e
Ruby Xu <139299636+lWhiteNightl@users.noreply.github.com>
2025-06-14 00:31:14 -0400

    add hamburger btn

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -17,3 +17,26 @@ function hideGreenBar() {
     greenBar.style.display = 'none';
     localStorage.setItem('greenBarClosed', 'true');
 }
+
+var burgerBtn = document.querySelector('#burger-btn');
+var mainNav = document.querySelector('#main-nav');
+var closeMainNav = document.querySelector('#close-main-nav-btn');
+
+burgerBtn.addEventListener('click', openMobleMenu);
+closeMainNav.addEventListener('click', closeMobileMenu);
+
+function openMobleMenu() {
+    mainNav.style.display = 'block';
+
+    burgerBtn.style.display = 'none';
+
+    closeMainNav.style.display = 'block';
+}
+
+function closeMobileMenu() {
+    mainNav.style.display = 'none';
+
+    burgerBtn.style.display = 'block';
+
+    closeMainNav.style.display = 'none';
+}
